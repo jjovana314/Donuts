52f59eca67c7c155f84dfdd768120336bbcc2eea
Jovana <jjovana314@gmail.com>
2020-08-25 11:28:37 +0200

    function generate_data updated

--- web/helper.py
+++ web/helper.py
@@ -17,30 +17,38 @@ def validate_schema(schema: dict, data: dict) -> None:
 
 
 list_data = []
+all_keys = ["id", "type", "name", "batters", "topping"]
 
 
-def data_getter(data: object):
+def generate_data(data: object):
     if isinstance(data, list):
         # iterate thru dictionaries
         for dictionary in data:
-            data_getter(dictionary)
+            generate_data(dictionary)
+    [id_current, type_current, name, batters, topping] = geters(data)
     # id, type, name, batter, topping
-    id_current = data.get("id", None)
-    type_current = data.get("type", None)
-    name = data.get("name", None)
-    batters = data.get("batters", None)
-    topping = data.get("topping", None)
+    # id_current = data.get("id", None)
+    # type_current = data.get("type", None)
+    # name = data.get("name", None)
+
+    # batters = data.get("batters", None)
+    # topping = data.get("topping", None)
 
     if batters is not None:
         batter = batters["batter"]
         for value in batter:
-            data_getter(value)
+            generate_data(value)
     if topping is not None:
         for value in topping:
-            data_getter(value)
+            generate_data(value)
 
     list_data.append((id_current, type_current, name))
 
+
+def geters(data: dict) -> list:
+    return [data.get(key, None) for key in all_keys]
+    # for key in keys:
+    #     list_return.append(data.get(key, None))
 
 with open("data_db.txt", "w") as f:
     f.write(str(list_data))
