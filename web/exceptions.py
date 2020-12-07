74c3d937590a62d8fae61cb12aabc2d2c52c0528
Jovana <jjovana314@gmail.com>
2020-12-07 22:15:16 +0100

    DontuDataError and InvalidValue classes added

--- web/exceptions.py
+++ web/exceptions.py
@@ -1,4 +1,10 @@
-class InvalidSchemaError(Exception):
+class DonutDataError(Exception):
+    """ General donut data exception. """
+
+
+class InvalidSchemaError(DonutDataError):
     """ Raised if schema is not valid. """
 
 
+class InvalidValue(DonutDataError, ValueError):
+    """ Raised if value in donut data is not valid. """
